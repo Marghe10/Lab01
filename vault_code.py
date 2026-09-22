2fd408ad73f1fafd3f3326c9b650df0fd4db0ec2
Marguerite Rossetti <s340927@studenti.polito.it>
2026-09-23 01:23:43 +0200

    lab 1 aggiornato a 22-09-26

--- vault_code.py
+++ vault_code.py
@@ -3,7 +3,7 @@ import random
 # Costanti del gioco
 CODICE_MIN = 1
 CODICE_MAX = 50
-TENTATIVI_BASE = 6
+TENTATIVI_INIZIO = 6
 TENTATIVI_MIN = 3
 MAX_LIVELLO = 3
 
@@ -13,9 +13,15 @@ def dai_indizio(tentativo, codice):
     # TODO
 
 
-def stampa_tentativi(n, usati):
+def stampa_tentativi(tentativi_disponibili, usati):
     """Stampa la riga dei tentativi: O = disponibile, X = già usato"""
     # TODO
+    print("tentativi disponibili:")
+    for y in range(0,usati):
+        print("X", end=" ")
+    for x in range(0,tentativi_disponibili):
+        print("O",end=" ")
+    print()
 
 
 def gestisci_livello(livello):
@@ -28,14 +34,21 @@ def gestisci_livello(livello):
     """
 
     # Inizializzazioni
-    n = TENTATIVI_BASE - livello
-    if n < TENTATIVI_MIN:
-        n = TENTATIVI_MIN
+    tentativi_tot_disponibili = TENTATIVI_INIZIO - livello
+    if tentativi_tot_disponibili < TENTATIVI_MIN:
+        tentativi_tot_disponibili = TENTATIVI_MIN
 
     codice = random.randint(CODICE_MIN, CODICE_MAX)
     usati = 0
+    tentativi_rimanenti=tentativi_tot_disponibili
 
     # TODO
+    stampa_tentativi(tentativi_rimanenti,usati)
+    in_gioco=input("inserire il numero da giocare")
+    if in_gioco == codice:
+        return True
+    else:
+        indizio=dai_indizio(tentativo, codice)
 
 
 def main():
@@ -44,7 +57,7 @@ def main():
 
     while livello <= MAX_LIVELLO:
         completato = gestisci_livello(livello)
-        if completato:
+        if completato is True:
             livello += 1
         else:
             break
